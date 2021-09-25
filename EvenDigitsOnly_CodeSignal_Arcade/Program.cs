dcdd90d39cd38970330fce7b06374bb8e309d5f5
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-25 13:01:51 +0400

    Добавьте файлы проекта.

---
+++ EvenDigitsOnly_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenDigitsOnly_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 24682;
+            bool x = evenDigitsOnly(n);
+            Console.WriteLine(x);
+
+        }
+
+        static bool evenDigitsOnly(int n)
+        {
+            int y = 0;
+            int g = 0;
+            string x = Convert.ToString(n);
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (Convert.ToInt32(x[i]) % 2 == 0)
+                {
+                    y = 1;   
+                }
+                else
+                {
+                    g = 1;
+                }
+            }
+            if (y == 1 && g == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
